3d6bdea3d4032d16701c3135dc80e6534da3cd88
useranon350 <useranon350@gmail.com>
2024-06-01 16:43:57 -0400

    Update Arcan and associated packages. Temporarily disable libunwind.
    Update arcan and cat9 to latest master and enable debug printing.
    
    Enable debugging arcan.
    
    Arcan fixes.
    
    Arcan patches.

--- pkgs/development/libraries/kmsxx/default.nix
+++ pkgs/development/libraries/kmsxx/default.nix
@@ -8,6 +8,10 @@
 , libdrm
 , fmt
 , libevdev
+, libGL
+, libX11
+, wayland
+, mesa
 , withPython ? false
 , python3Packages
 }:
@@ -26,12 +30,23 @@ stdenv.mkDerivation {
 
   # Didn't detect pybind11 without cmake
   nativeBuildInputs = [ meson ninja pkg-config ] ++ lib.optionals withPython [ cmake ];
-  buildInputs = [ libdrm fmt libevdev ]
+  buildInputs = [ libGL wayland mesa libX11 libdrm fmt libevdev ]
     ++ lib.optionals withPython (with python3Packages; [ python pybind11 ]);
 
   dontUseCmakeConfigure = true;
 
-  mesonFlags = lib.optional (!withPython) "-Dpykms=disabled";
+  mesonFlags = [
+    "-Dkmscube=true"
+  ] ++ lib.optional (!withPython) "-Dpykms=disabled";
+
+  postPatch = ''
+    substituteInPlace ./utils/meson.build \
+      --replace-fail "install : false" "install : true"
+    substituteInPlace ./kmscube/meson.build \
+      --replace-fail ": kmscube_deps)" ": kmscube_deps, install : true)"
+    substituteInPlace ./kmscube/cube-gbm.cpp \
+      --replace-fail "GBM_BO_USE_SCANOUT | GBM_BO_USE_RENDERING" "0"
+  '';
 
   meta = with lib; {
     description = "C++11 library, utilities and python bindings for Linux kernel mode setting";
